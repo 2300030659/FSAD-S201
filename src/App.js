310b1da5dd48d2143b4194572ed27aed4d3bc7b1
2300030659 <2300030659@kluniversity.in>
2025-08-05 20:09:55 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -4,19 +4,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header 
+        className="App-header" 
+        style={{ 
+          display: 'flex', 
+          flexDirection: 'column', 
+          alignItems: 'center', 
+          justifyContent: 'center', 
+          minHeight: '100vh', 
+          backgroundColor: '#282c34', 
+          color: 'white', 
+          textAlign: 'center',
+          padding: '20px'
+        }}
+      >
+        <img 
+          src={logo} 
+          className="App-logo" 
+          alt="logo" 
+          style={{ height: '100px', marginBottom: '20px' }} 
+        />
+
+        <h1 
+          style={{ 
+            fontSize: '24px', 
+            lineHeight: '1.6', 
+            marginBottom: '10px'
+          }}
         >
-          Learn React
-        </a>
+          FSAD Project Deployment<br />
+          Done by ID: 2300030659<br />
+          Name: Syed Mastan Vali<br />
+          Section: 201
+        </h1>
       </header>
     </div>
   );
